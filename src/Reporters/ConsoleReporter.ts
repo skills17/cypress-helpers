58ae9df8f1533075ebe04250742bce67e0ffb9f0
Cyril Wanner <info@cyr.li>
2021-01-11 00:24:49 +0000

    Combine multiple spec files in one output

--- src/Reporters/ConsoleReporter.ts
+++ src/Reporters/ConsoleReporter.ts
@@ -3,29 +3,37 @@ import TaskConfig from '@skills17/task-config';
 import TestResultPrinter from '@skills17/test-result-printer';
 import { TestRun } from '@skills17/test-result'; // eslint-disable-line import/no-extraneous-dependencies
 
-const { EVENT_RUN_BEGIN, EVENT_RUN_END, EVENT_TEST_END } = Mocha.Runner.constants;
+const { EVENT_TEST_END } = Mocha.Runner.constants;
 
 export default class ConsoleReporter {
-  protected testRun?: TestRun;
+  private static testRun: TestRun;
 
   constructor(runner: Mocha.Runner) {
-    const config = new TaskConfig();
-    config.loadFromFileSync();
+    // cypress starts a completely new test run per spec file.
+    // so we have to store the test run in a static variable and listen on the `beforeExit` event
+    // of the process to output the result before the process finishes.
+    if (!ConsoleReporter.testRun) {
+      const config = new TaskConfig();
+      config.loadFromFileSync();
+      ConsoleReporter.testRun = config.createTestRun();
 
-    runner
-      .once(EVENT_RUN_BEGIN, () => {
-        this.testRun = config.createTestRun();
-      })
-      .on(EVENT_TEST_END, this.onTestEnd.bind(this))
-      .on(EVENT_RUN_END, this.onRunEnd.bind(this));
+      process.on('exit', this.onRunEnd.bind(this));
+    }
+
+    runner.on(EVENT_TEST_END, this.onTestEnd.bind(this));
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public getTestRun(): TestRun {
+    return ConsoleReporter.testRun;
   }
 
   public onTestEnd(test: Mocha.Test): void {
-    this.testRun?.recordTest(test.titlePath().join(' > '), false, test.isPassed());
+    this.getTestRun().recordTest(test.titlePath().join(' > '), false, test.isPassed());
   }
 
   public onRunEnd(): void {
-    const printer = new TestResultPrinter(this.testRun as TestRun);
+    const printer = new TestResultPrinter(this.getTestRun());
     printer.print({ printFooter: false });
   }
 }
